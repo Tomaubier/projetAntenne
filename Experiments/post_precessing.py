6c1a69be711394237565f70ac8024af59ba3ba12
Tomaubier <tom.aubierlaure@orange.ffr>
2019-05-11 00:05:30 +0200

    Savefig

--- Experiments/post_precessing.py
+++ Experiments/post_precessing.py
@@ -8,15 +8,15 @@ import numpy as np
 
 2**13
 
-steering_mic, Fs, Ntfd, freqBand, interp = 1, 44.1e3, 176128, [125, 1500], (True, 100)
+steering_mic, Fs, Ntfd, freqBand, interp = 1, 44.1e3, 176128, [60, 2285], (True, 100)
 
 antenne = da.DifferentialArray(steering_mic, Fs, Ntfd, freqBand, interp )
 gains = antenne.filter_matrix()
 Fk = np.arange(Ntfd)*Fs/Ntfd
 
-parentPath = os.path.abspath('.')
+parentPath = os.path.abspath('./Experiments')
 dataDirectoryPath = os.path.join(parentPath, 'Data')
-MEASURE_NAME, ANGLE_STEP = 'ChirpWindow', 5
+MEASURE_NAME, ANGLE_STEP = 'ChirpNoWindow', 5
 fexp = 1000
 
 
@@ -40,39 +40,24 @@ for ii, angle in enumerate(angles):
     Z[ii, :] = np.abs(Normed_Output)
     value41[ii] = np.abs(Normed_Output[fexp])
 
+# %% Polar plot
 
-    # fig, ax = plt.subplots(nrows=1)
-    # ax.plot(Fk, np.abs(Normed_Output))
-    # ax.set_title("Degrés {}".format(angle))
-    # ax.set_ylabel('Amplitude')
-    # ax.set_xlabel('Frequence en Hz')
-    # ax.set_xlim(freqBand)
-    # ax.set_ylim([0, 1])
-    # plt.show()
-    #wav.write(os.path.join(Path_New, name), Fs, data)
+ax = plt.subplot(111, projection='polar') #, figsize=(12, 5))
+ax.set_theta_zero_location('E')
+ax.plot(np.deg2rad(angles), Z[:, np.where(Fk >= 500)[0][0]])
+plt.grid(True)
+plt.show()
+
+# %%
 
 index_freqBand = [np.where(Fk >= f)[0][0] for f in freqBand]
-Z = Z[:, index_freqBand[0]:index_freqBand[1]]
+Zrolled = np.roll(Z[:, index_freqBand[0]:index_freqBand[1]], Z.shape[0] // 2, axis=0)
 
-# %%
-%%time
-plt.subplots(nrows=1, figsize=(10, 5))
-plt.pcolormesh(X, Y, Z, cmap='RdBu', vmax=1)
-plt.xlim(freqBand)
-plt.xlabel(r'Fréquence en Hz')
-plt.ylabel(r'Angle en degré')
-plt.colorbar()
-plt.savefig(os.path.join(parentPath, 'Figures', 'directivityFreq1.pdf'))
-plt.show()
-
-# %%
-%%time
 plt.subplots(nrows=1, figsize=(12, 5))
-plt.imshow(Z, cmap='RdBu', vmin=0, vmax=1, extent=[freqBand[0], freqBand[1], 0, 355], aspect=2.5, interpolation='nearest', origin='lower')
-plt.xlabel(r'Fréquence en Hz')
-plt.ylabel(r'Angle en degré')
+plt.imshow(Zrolled, cmap='RdBu_r', vmin=0, vmax=1, extent=[freqBand[0], freqBand[1], -175, 180], aspect=3, interpolation='nearest', origin='lower')
+plt.xlabel(r'Fréquence (Hz)')
+plt.ylabel(r'Angle (°)')
 plt.colorbar()
-plt.savefig(os.path.join(parentPath, 'Figures', 'directivityFreq.pdf'))
+plt.tight_layout()
+plt.savefig(os.path.join(parentPath, 'Figures', '2-2-AngleFreq.eps'))
 plt.show()
-
-# plt.savefig(os.path.join(parentPath, 'Figures', 'directivityFreq.pdf'))
